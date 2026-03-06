1919b418bb08f04c724a04cd27cf0b577d59a500
TMcCann2019 <timothysmccann@gmail.com>
2026-03-06 01:12:05 -0500

    working errors with frontend

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS quotes (
-  id SERIAL PRIMARY KEY,
-  quote TEXT NOT NULL,
-  author VARCHAR(255)
-);ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    id SERIAL PRIMARY KEY,
+    quote TEXT NOT NULL,
+    author VARCHAR(255)
+);
 
-INSERT INTO quotes(quote, author)
-VALUES("Be the change you want to see in the world.", "Ghandi"), ("Master patterns in life, and  you'll never suffer is the secret","someone");
+INSERT INTO quotes (quote, author) VALUES
+  ('Be the change you want to see in the world.', 'Gandhi'),
+  ('Master patterns in life, and you''ll never suffer is the secret', 'someone');
