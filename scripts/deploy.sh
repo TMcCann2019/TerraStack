8268b2826e7f6272a976dc9bfda21fb9e08b6c64
TMcCann2019 <47599988+TMcCann2019@users.noreply.github.com>
2026-03-06 20:28:44 -0500

    adding export and remaining full commands to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,12 @@
-minikube start
-eval $(minikube docker-env)
+export MINIKUBE_ROOTLESS=false
+minikube start --driver=docker --container-runtime=docker
+minikube addons enable registry
+eval $(minikube -p minikube docker-env)
 
 docker build -t terrastack-backend ./docker/backend
 docker build -t terrastack-frontend ./docker/frontend
 docker build -t terrastack-nginx ./docker/nginx
+
+kubectl create ns terrastack
 
 helm install terrastack ./charts/terrastack
